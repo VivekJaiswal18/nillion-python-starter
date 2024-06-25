5b1c07f39c7c9d0f9538f55861f38139d51da9b3
VivekJaiswal18 <vivekjaiswalfrl@gmail.com>
2024-06-25 15:20:45 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,20 @@
 from nada_dsl import *
-
+import random
 
 def nada_main():
     party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    # Define an array of jokes
+    jokes = [
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "Why did the scarecrow win an award? Because he was outstanding in his field!",
+        "Why don't skeletons fight each other? They don't have the guts!",
+        "What do you call fake spaghetti? An impasta!",
+        "Why was the math book sad? Because it had too many problems!"
+    ]
 
-    return [Output(my_int1, "my_output", party1)]
+    # Choose a random joke from the array
+    selected_joke = random.choice(jokes)
+
+    # Return the selected joke as the output
+    return [Output(selected_joke, "joke_output", party1)]
